39f05023e40b6a17a470be36a7b837418a3f9c6b
Ps0ke <ps0ke.mail@gmail.com>
2012-04-24 21:26:33 +0200

    Fix fancybox plugin with absolute paths and supporting an additional syntax.

--- plugins/FancyBox/php_mdconverter-html.php
+++ plugins/FancyBox/php_mdconverter-html.php
@@ -1,3 +1,4 @@
 <?php
 $html = preg_replace('/<img src="(.+)" alt="(.*)">/', '<a class="fancybox" href="${1}"><img src="${1}" alt="${2}"></a>', $html);
+$html = preg_replace('/<img alt="(.*)" src="(.+)">/', '<a class="fancybox" href="${2}"><img src="${2}" alt="${1}"></a>', $html);
 ?>
