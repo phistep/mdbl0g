b53824da82ef472217eb632fd2988db4692668fc
Ps0ke <ps0ke.mail@gmail.com>
2012-08-27 22:23:12 +0200

    Cleaned up function code for `post_details'.

--- core/functions/functions.php
+++ core/functions/functions.php
@@ -34,9 +34,7 @@ function post_details($filename){
 	$post['prettyid'] = date('Y/m/d/H/i/', $post['timestamp']).to_url($post['title']);
 	
 	$trimmedcontent = array_slice($content, 1);
-	$post['content'] = '';
-	foreach($trimmedcontent as $line)
-		$post['content'] .= $line;
+	$post['content'] = implode("\n", $trimmedcontent);
 
 	foreach(glob(BASE_PATH."plugins/*/php_functions-post_details.php") as $filename){include $filename;}
 
